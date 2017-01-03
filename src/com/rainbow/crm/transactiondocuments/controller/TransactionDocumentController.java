5495e3341b5fac8c38bc8399022ad7d024365f5a
rainbowcrm <unnikrishnan.dn@gmail.com>
2017-01-04 00:06:15 +0530

    document print pages

--- src/com/rainbow/crm/transactiondocuments/controller/TransactionDocumentController.java
+++ src/com/rainbow/crm/transactiondocuments/controller/TransactionDocumentController.java
@@ -8,6 +8,8 @@ import com.rainbow.crm.common.SpringObjectFactory;
 import com.rainbow.crm.database.LoginSQLs;
 import com.rainbow.crm.distributionorder.model.DistributionOrder;
 import com.rainbow.crm.distributionorder.service.IDistributionOrderService;
+import com.rainbow.crm.sales.model.Sales;
+import com.rainbow.crm.sales.service.ISalesService;
 import com.rainbow.crm.transactiondocuments.model.TransactionDocument;
 import com.techtrade.rads.framework.context.IRadsContext;
 import com.techtrade.rads.framework.controller.abstracts.GeneralController;
@@ -23,7 +25,10 @@ public class TransactionDocumentController extends GeneralController{
 		PageResult result = new PageResult();
 		TransactionDocument transDoc= (TransactionDocument)object ;
 		if("SalesInvoice".equalsIgnoreCase(transDoc.getDocType())) {
-			
+			ISalesService service = (ISalesService)SpringObjectFactory.INSTANCE.getInstance("ISalesService") ;
+			Sales sales = (Sales) service.getById(transDoc.getPk());
+			String doc = service.generateInvoice(sales, (CRMContext) getContext());
+			transDoc.setDocument(doc);		
 		}else if("ShippingLabel".equalsIgnoreCase(transDoc.getDocType())) {
 			IDistributionOrderService service = (IDistributionOrderService)SpringObjectFactory.INSTANCE.getInstance("IDistributionOrderService") ;
 			DistributionOrder order = (DistributionOrder)service.getById(transDoc.getPk());
