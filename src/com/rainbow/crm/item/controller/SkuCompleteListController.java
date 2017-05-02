3621b3d6013faa9dc2a703d5e71bc0a60fd96787
rainbowcrm <unnikrishnan.dn@gmail.com>
2017-05-02 13:49:39 +0530

    returning item images too

--- src/com/rainbow/crm/item/controller/SkuCompleteListController.java
+++ src/com/rainbow/crm/item/controller/SkuCompleteListController.java
@@ -3,12 +3,14 @@ package com.rainbow.crm.item.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.rainbow.crm.common.CRMAppConfig;
 import com.rainbow.crm.common.CRMContext;
 import com.rainbow.crm.common.IBusinessService;
 import com.rainbow.crm.config.service.ConfigurationManager;
 import com.rainbow.crm.item.dao.ItemImageSQL;
 import com.rainbow.crm.item.model.Sku;
 import com.rainbow.crm.item.model.SkuComplete;
+import com.rainbow.crm.logger.Logwriter;
 import com.techtrade.rads.framework.filter.Filter;
 import com.techtrade.rads.framework.model.abstracts.ModelObject;
 import com.techtrade.rads.framework.ui.components.SortCriteria;
@@ -28,13 +30,19 @@ public class SkuCompleteListController extends SkuListController {
 				SkuComplete itemComplete = new SkuComplete((Sku) sku);
 				List<String > imageURLs = ItemImageSQL.getAllItemImages(itemComplete.getId());
 				if(!Utils.isNullList(imageURLs)) {
-					String path = ConfigurationManager.getConfig(
-							ConfigurationManager.IMAGE_SERVER_URL, (CRMContext)getContext());
-					itemComplete.setImage1URL(path + "/" + imageURLs.get(0).toString());
+					/*String path = ConfigurationManager.getConfig(
+							ConfigurationManager.IMAGE_SERVER_URL, (CRMContext)getContext());*/
+					try  {
+					String path = CRMAppConfig.INSTANCE.getProperty("doc_server");
+					String companyCode = ((CRMContext) getContext()).getLoggedinCompanyCode();
+					itemComplete.setImage1URL(path +"/" + companyCode+ "/" + "itemimages/" + "/" + imageURLs.get(0).toString());
 					if(imageURLs.size() > 1) 
-						itemComplete.setImage2URL(path + "/" + imageURLs.get(1).toString());
+						itemComplete.setImage2URL(path +"/" + companyCode+ "/" + "itemimages/" + "/" + imageURLs.get(1).toString());
 					if(imageURLs.size() > 2) 
-						itemComplete.setImage3URL(path + "/" + imageURLs.get(2).toString());
+						itemComplete.setImage3URL(path +"/" + companyCode+ "/" + "itemimages/" + "/" + imageURLs.get(2).toString());
+					}catch(Exception ex) {
+						Logwriter.INSTANCE.error(ex);
+					}
 				}
 				itemWithImgs.add(itemComplete);
 			} );
