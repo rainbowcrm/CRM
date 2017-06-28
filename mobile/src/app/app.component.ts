d3c4a366f1e1078222f40cca69d2f28c0f2b7b84
Sandeep Pande <sandy2spicy@gmail.com>
2017-06-28 10:29:07 +0530

    Fixed splash screen issue, notif fix and hardware back button

--- mobile/src/app/app.component.ts
+++ mobile/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav, AlertController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { LoginPage } from '../pages/login/login';
 import { HomePage } from '../pages/home/home';
@@ -13,10 +13,49 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 })
 export class PSApp {
   rootPage = LoginPage;
-
-  constructor(platform: Platform, private statusBar: StatusBar, private splashScreen: SplashScreen) {
+  alert: any;
+  @ViewChild(Nav) nav: Nav;
+  constructor(private platform: Platform, private statusBar: StatusBar, 
+                 private splashScreen: SplashScreen, private alertCtrl: AlertController) {
     platform.ready().then(() => {
             this.statusBar.styleDefault();
+            this.splashScreen.hide();
+
+        platform.registerBackButtonAction(() => {
+            // get current active page
+            if(this.nav.canGoBack()){
+              this.nav.pop();
+            }else{
+              if(this.alert){ 
+                this.alert.dismiss();
+                this.alert =null;     
+              }else{
+                this.showAlert();
+               }
+            }
+        });
     });
   }
+  showAlert() {
+      this.alert = this.alertCtrl.create({
+        title: 'Exit?',
+        message: 'Do you want to exit the app?',
+        buttons: [
+          {
+            text: 'Cancel',
+            role: 'cancel',
+            handler: () => {
+              this.alert =null;
+            }
+          },
+          {
+            text: 'Exit',
+            handler: () => {
+              this.platform.exitApp();
+            }
+          }
+        ]
+      });
+      this.alert.present();
+    }
 }
--- mobile/src/pages/login/login.ts
+++ mobile/src/pages/login/login.ts
@@ -48,7 +48,7 @@ export class LoginPage {
     }
     this.response = response.dataObject;
     this.storage.set('user', response.dataObject.Username);
-     this.storage.set('division', response.dataObject.LoggedInDivision);
+    this.storage.set('division', response.dataObject.LoggedInDivision);
     this.http.setAuthToken(response.dataObject.AuthToken);
     this.navCtrl.setRoot(HomePage);
     
